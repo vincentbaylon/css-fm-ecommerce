5d4c1e932c8c653f9ed76478a5a51bd72bd425b3
Vincent <vincentbaylon@gmail.com>
2022-02-18 20:16:42 -0800

    Finished litebox

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let count = 0;
+let active = 1;
 let cartArr = [];
 let product = {
    name: "Fall Limited Edition Sneakers",
@@ -116,6 +117,29 @@ document.querySelector(".close-backdrop").addEventListener("click", () => {
    backdrop.style.visibility = "hidden";
 });
 
+document
+   .querySelector(".main__pictures--large")
+   .addEventListener("click", () => {
+      const litebox = document.querySelector(".litebox");
+      const largeImg = document.querySelector(".litebox__img--large");
+
+      litebox.style.visibility = "visible";
+      litebox.style.opacity = "1";
+      largeImg.src = `images/image-product-${active}.jpg`;
+
+      liteboxRemoveActive();
+      liteboxAddActive(active);
+   });
+
+document
+   .querySelector(".litebox__icon--close")
+   .addEventListener("click", () => {
+      const litebox = document.querySelector(".litebox");
+
+      litebox.style.visibility = "hidden";
+      litebox.style.opacity = "0";
+   });
+
 // FUNCTIONS
 function updateLabel() {
    const label = document.querySelector(".counter__label");
@@ -129,11 +153,28 @@ function removeActive() {
    activeImg.classList.remove("active-img");
 }
 
+function liteboxRemoveActive() {
+   const active = document.querySelector(".litebox-active");
+   const activeImg = document.querySelector(".litebox-active-img");
+   const activeFigure = document.querySelector(".litebox-active-figure");
+   active.classList.remove("litebox-active");
+   activeImg.classList.remove("litebox-active-img");
+   activeFigure.classList.remove("litebox-active-figure");
+}
+
 function addActive(num) {
    const img = document.querySelector(`.main__pictures--thumb-${num}`);
    const imgThumb = document.querySelector(`.main__img--${num}`);
    img.classList.add("active");
    imgThumb.classList.add("active-img");
+}
+
+function liteboxAddActive(num) {
+   const img = document.querySelector(`.litebox__thumb-holder-${num}`);
+   const imgThumb = document.querySelector(`.litebox__img--thumb-${num}`);
+   img.classList.add("litebox-active");
+   img.classList.add("litebox-active-figure");
+   imgThumb.classList.add("litebox-active-img");
 }
 
 // RENDER FUNCTIONS
@@ -149,12 +190,33 @@ function imageEventListeners() {
 
          removeActive();
          addActive(value);
+         active = value;
+      });
+   });
+}
+
+function liteboxImageEventListeners() {
+   [1, 2, 3, 4].forEach((each) => {
+      const img = document.querySelector(`.litebox__thumb-holder-${each}`);
+
+      img.addEventListener("click", () => {
+         const value = img.getAttribute("value");
+
+         const largeImg = document.querySelector(".litebox__img--large");
+         largeImg.src = `images/image-product-${value}.jpg`;
+
+         liteboxRemoveActive();
+         liteboxAddActive(value);
+         active = value;
+
+         console.log("LITEBOX");
       });
    });
 }
 
 function initialRender() {
    imageEventListeners();
+   liteboxImageEventListeners();
 }
 
 initialRender();
